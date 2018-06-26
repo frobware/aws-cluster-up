c4314690d37ea214f761ca4f0a11fff436264a3c
Andrew McDermott <aim@frobware.com>
2018-06-26 13:55:08 +0100

    Remove AMI image references from defaults
    These should now be specified in the terraform plan files.

--- examples/aws/us-east-1.bash
+++ examples/aws/us-east-1.bash
@@ -1,9 +1,5 @@
 set -a
 
-: ${ACU_AMI_IMAGE_INFRA:=ami-0af8ebba55e71b379}
-: ${ACU_AMI_IMAGE_MASTER:=ami-0af8ebba55e71b379}
-: ${ACU_AMI_IMAGE_NODE:=ami-0af8ebba55e71b379}
-
 : ${ACU_INSTANCE_TYPE:=m4.large}
 : ${ACU_KEY_NAME:=libra}
 : ${ACU_REGION:=us-east-1}
